061c6fc4d6ab0cdb2d7a6162fa72448b15d31113
David Jackson <djackson@gkcorp.com>
2023-11-16 16:54:34 -0500

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,7 @@
 import logging
 import logging.handlers
 import os
+import json
 
 import requests
 
@@ -20,27 +21,10 @@ if __name__ == "__main__":
 
     wmo = "723100"
     
-    r = requests.post(
-        'http://ashrae-meteo.info/v2.0/request_meteo_parametres.php', 
-        json = {
-            "wmo": wmo, 
-            "ashrae_version": "2021", 
-            "si_ip": "IP"
-        },
-        headers = {
-            "accept": "*/*",
-            "accept-language": "en-US,en;q=0.9",
-            "cache-control": "no-cache",
-            "content-type": "application/x-www-form-urlencoded",
-            "pragma": "no-cache",
-            "Referer": "http://ashrae-meteo.info/v2.0/",
-            "Referrer-Policy": "strict-origin-when-cross-origin"
-        }
-    )
-    
-    logger.info(f'WMO {wmo}: Request Status Code: {r.status_code}')
-    
-    if r.status_code == 200:
-        data = r.json()
-        place = data["place"]
-        logger.info(f'WMO {wmo}: {place}')
+    with open('stations.json', 'r') as f:
+        stations = json.load(f)
+
+    filtered_stations = filter(lambda x: x['wmo'] == wmo, stations)
+
+    for (x in filtered_stations):
+        logger.info(f'WMO {wmo}: {json.dumps(x)}')
