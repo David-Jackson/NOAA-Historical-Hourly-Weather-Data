1d37f2372c20f29a44f371ae9ae05ed66a1b3084
David Jackson <djackson@gkcorp.com>
2023-11-16 15:40:08 -0500

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -20,7 +20,25 @@ if __name__ == "__main__":
 
     wmo = "723100"
     
-    r = requests.post('http://ashrae-meteo.info/v2.0/request_meteo_parametres.php', json={"wmo": wmo, "ashrae_version": "2021", "si_ip": "IP"})
+    r = requests.post(
+        'http://ashrae-meteo.info/v2.0/request_meteo_parametres.php', 
+        json = {
+            "wmo": wmo, 
+            "ashrae_version": "2021", 
+            "si_ip": "IP"
+        },
+        headers = {
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9",
+            "cache-control": "no-cache",
+            "content-type": "application/x-www-form-urlencoded",
+            "pragma": "no-cache",
+            "Referer": "http://ashrae-meteo.info/v2.0/",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        }
+    )
+    
+    logger.info(f'WMO {wmo}: Request Status Code: {r.status_code}')
     
     if r.status_code == 200:
         data = r.json()
